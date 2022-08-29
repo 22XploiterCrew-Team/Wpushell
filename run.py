53b91b32980035e0515eca7b5b6bfe6b26bcb1d9
RandsX <andrevirdiaz@gmail.com>
2022-08-30 00:37:54 +0700

    check executable file

--- run.py
+++ run.py
@@ -17,7 +17,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
+import os
+import sys
+
 from wpushell.cli import run
 
 if __name__ == '__main__':
-    run()
+    if os.path.basename(sys.argv[0]) == os.path.basename(__file__):
+        if not os.access(sys.argv[0], os.X_OK):
+            os.chmod(sys.argv[0], 0o755)
+        run()
