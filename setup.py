143e725a2ddfc183d6be110cb0ec9af940b60291
RandsX <andrevirdiaz@gmail.com>
2023-05-22 22:32:40 +0700

    fix bug

--- setup.py
+++ setup.py
@@ -15,28 +15,37 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+Install packages as defined in this file into the Python environment.
 """
+from setuptools import setup, find_packages
 
-from wpushell.__version__ import __version__
-from setuptools import setup
-from setuptools import find_packages
-
-with open('requirements.txt') as file:
-    requirements = file.read().splitlines()
+# The version of this tool is based on the following steps:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+VERSION = {}
+with open("./wpushell/__version__.py") as file:
+    exec(file.read(), VERSION)
 
 setup(
-    name='wpushell',
-    version=__version__,
-    description='Wpushell is a tool used to upload a backdoor shell to a site that uses a WordPress Content Management System with a simple and fast process.',
-    author='RandsX',
-    author_email='andrewvirdiaz@gmail.com',
-    keywords='wordpress, upload, backdoor, shell',
+    name="wpushell",
+    author="RandsX",
+    author_email="andrevirdiaz@gmail.com",
+    url="https://github.com/22XploiterCrew-Team/Wpushell",
+    description="Wpushell is a tool used to upload a backdoor shell to a site that uses a WordPress Content Management System with a simple and fast process.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    version=VERSION.get("__version__", "0.0.0"),
+    packages=find_packages(where="."),
+    install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
-        'console_scripts': [
-            'wpushell = wpushell.__init__:run'
+        "console_scripts": [
+            "wpushell=wpushell.__init__:run",
         ]
     },
-    packages=find_packages(),
-    install_requires=requirements,
-    include_package_data=True
+    classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Security",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+    ],
 )
